322222e93076a485ccaf895354fc541f25a2adcc
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-05-19 18:51:39 +0900

    operator

---
+++ src/section03/Operator01.java
@@ -0,0 +1,58 @@
+package section03;
+/*
+ * 연산자(Operator)
+ * 	프로그램에서 데이터를 처리하여 산출하는것을 연산이라하고,
+ * 	연산에 사용되는 표시나 기호를 연산자(operator)라고 한다.
+ * 	
+ * 1. 산술연산자
+ * 	+, -, *, /, %
+ *
+ *	
+ *  숫자	+ 숫자 -> 숫자
+ *  !문자에서 +는 연결연산자 이다.
+ *  문자 + 문자 or 숫자 -> 문자 연결
+ *   
+ * 
+ * Ctrl + s 저장
+ * Ctrl + Shift + s 전체저장
+ */
+public class Operator01 {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 5;
+		
+		// 두수의 합을 구해서 sum이라는 int형 변수에 대입
+		int sum = num1 + num2;
+		System.out.println("sum: " + sum);
+		
+		String result = num1 + "+" + num2 + "=" + sum;
+		System.out.println(result);
+		
+		// 두수의 곱을 구해서 multiply라는 변수에 대입
+		int multiply = num1 * num2;
+		System.out.println("두수의 곱: " + multiply);
+		
+		// 나누기
+		int divide = 10/3;
+		System.out.println("10/3 : " + divide);
+		
+		
+		int divide2 = num1/num2;
+		System.out.println("num1/num2: " + divide2);
+
+		// 나머지 값
+		int resultNum = 9 % 5;
+		System.out.println("9 % 5: " + resultNum);
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
