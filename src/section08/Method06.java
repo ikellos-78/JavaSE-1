cd06ec1bf38433d9a78361194cd0f27c1abe18a7
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-05-30 21:55:07 +0900

    메소드

---
+++ src/section08/Method06.java
@@ -0,0 +1,26 @@
+package section08;
+
+public class Method06 {
+	public static void main(String[] args) {
+
+		String name = getName();
+		System.out.println("name: " + name);
+		
+		System.out.println("name: " + getName());
+		
+	}
+	
+	// 6. 인자 x 리턴 o -> 메소드 실행하여 값은 반환한다.
+	public static String getName() {
+		String name = "피카츄";
+		return name;
+	}
+
+}
+
+
+
+
+
+
+
---
+++ src/section08/Method07.java
@@ -0,0 +1,27 @@
+package section08;
+
+public class Method07 {
+	
+	public static void main(String[] args) {
+		
+		int result = add(10, 5);
+		System.out.println("result: " + result);
+	}
+	
+	// 7. 인자 o 리턴 o -> 인자값을 받아 실행한뒤 반환값을 준다.
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	// 오버로딩 메소드
+	public static int add(int a, int b, int c) {
+		return a + b + c;
+	}
+
+}
+
+
+
+
+
+
